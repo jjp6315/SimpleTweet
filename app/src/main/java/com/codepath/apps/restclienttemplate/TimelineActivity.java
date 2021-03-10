01d6719fc8514e73b8b8f1c4a8d931b667c00bf1
jjp6315 <jjp6315@psu.edu>
2021-03-09 23:11:55 -0500

    Finished without text count

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -117,6 +117,7 @@ public class TimelineActivity extends AppCompatActivity {
             tweets.add(0, tweet);
             // Update the adapter
             adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
